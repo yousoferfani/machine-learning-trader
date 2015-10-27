bc527da11c700df062ccfd276265157437157b79
Maxwell Margenot <dishoftheday@dishoftheday-VirtualBox.(none)>
2015-10-27 15:44:40 -0400

    Modified main.py to handle the data set

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import datetime
 import google_quote
 import neural_net_trader
 
+data = google_quote.GoogleIntradayQuote('tsla', 300, 30)
+data.write_csv("data.csv")
+data = pd.read_csv("data.csv", names = ["SYMBOL", "DATE", "TIME", "OPEN", "HIGH", "LOW", "CLOSE", "VOLUME"])
+
+net = neural_net_trader.NeuralNet(data.CLOSE)
